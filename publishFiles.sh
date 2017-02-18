70ebfd4a83d2a41f0b13d9f82766568ce9c4304b
Alex Landau <alandau@cs.stanford.edu>
2017-02-17 22:17:56 -0800

    Add git commands to publish script

--- publishFiles.sh
+++ publishFiles.sh
@@ -4,9 +4,14 @@ set -e
 
 ./checkEnvironmentVariables.sh
 
+(cd ../gdl-perf/ && rm -rf analyses/ && ./gradlew perfAnalysis)
 rm -rf docs/analyses
 cp -r ../gdl-perf/analyses docs/
 
 (cd ../alloy && ./gradlew :Alloy:uctCharts :Alloy:uctCharts2)
 cp ../alloy/Alloy/abfResultCharts.html docs/
 cp -r ../alloy/Alloy/uctViz docs/
+
+git add docs/
+git commit -m "Updating website"
+git push origin HEAD:master
