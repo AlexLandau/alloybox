bb8378d5a9aa6386a6d6c0c4d3789d2802807bf7
Alex Landau <alandau@cs.stanford.edu>
2017-08-30 20:31:08 -0700

    Stop generating Alloy-based UCT charts.

--- publishFiles.sh
+++ publishFiles.sh
@@ -8,9 +8,9 @@ set -e
 rm -rf docs/analyses
 cp -r ../gdl-perf/analyses docs/
 
-(cd ../alloy && ./gradlew :Alloy:uctCharts)
+# (cd ../alloy && ./gradlew :Alloy:uctCharts)
 # (cd ../alloy && ./gradlew :Alloy:uctCharts2)
-cp ../alloy/Alloy/abfResultCharts.html docs/
+# cp ../alloy/Alloy/abfResultCharts.html docs/
 # cp -r ../alloy/Alloy/uctViz docs/
 
 (cd ../game-ai-research && ./gradlew writeReports)
